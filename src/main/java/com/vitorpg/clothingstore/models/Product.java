d43c742bb6eda4c4fa1377fbc9e4a54388202809
Vitor Paulino <vitorpaulinog@gmail.com>
2024-12-22 18:17:20 -0300

    Add: Product entity

--- src/main/java/com/vitorpg/clothingstore/models/Product.java
+++ src/main/java/com/vitorpg/clothingstore/models/Product.java
@@ -1,4 +1,19 @@
 package com.vitorpg.clothingstore.models;
 
+import com.vitorpg.clothingstore.models.enums.Gender;
+
+import java.util.List;
+
 public class Product {
+    private Long id;
+    private String name;
+    private Category category;
+    private Style style;
+    private List<byte[]> images;
+    private Gender gender;
+    private Size size;
+    private Color color;
+    private Material material;
+    private Long amount;
+    private Double price;
 }
