87379e2f70281d29cda627c1432fde2f4dd53bad
Vitor Paulino <vitorpaulinog@gmail.com>
2024-12-22 18:15:19 -0300

    Add: Size entity

---
+++ src/main/java/com/vitorpg/clothingstore/models/Size.java
@@ -0,0 +1,9 @@
+package com.vitorpg.clothingstore.models;
+
+import com.vitorpg.clothingstore.models.enums.SizeType;
+
+public class Size {
+    private Long id;
+    private String value;
+    private SizeType sizeType;
+}
